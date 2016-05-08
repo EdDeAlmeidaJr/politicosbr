0a291e90aaf99c12c1119bd289ccc26efe592cfc
Edvaldo Silva de Almeida Júnior <edvaldoajunior@gmail.com>
2016-05-08 01:22:08 -0300

    Acrescentados os deputados estaduais do Paraná

---
+++ lib/politicosbr/constants.rb
@@ -0,0 +1,13 @@
+module PoliticosBR
+
+  ## URL to get the spreadesheet with the list of federal representatives
+  DEPUTADOS_URL = 'http://www.camara.gov.br/internet/deputado/deputado.xls'
+  
+  ## URL to get the list the senators in Brazilian congress
+  SENADORES_URL = 'http://www25.senado.leg.br/web/senadores/em-exercicio/-/e/por-nome'
+  
+  ## Hash containing the list of URLs to get the lists of state representatives in Brazilian states
+  DEPS_ESTADUAIS = { pr: 'http://www.alep.pr.gov.br/deputados' }
+  
+  
+end
